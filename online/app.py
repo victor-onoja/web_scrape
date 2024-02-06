6f7b00a2f126c9963e9bfae1d9a8e92c3cd1c637
victor-onoja <odohvictor47@gmail.com>
2024-02-06 12:24:20 +0100

    prepared for codes

--- online/app.py
+++ online/app.py
@@ -7,7 +7,7 @@ def get_services(link):
     jobs_list = soup.find('ul', class_='jobs-list')
     job = jobs_list.find_all('a')
     for j in job:
-        print(j['title'])
+        print("'" + j['title'] + "',")
 
 if __name__ == '__main__':
     user_link = input("Please provide the link: ")
